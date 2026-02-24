2267696218245e8acc77e6e349363c171bbf4cea
maker565 <oceanoantarticoagua@gmail.com>
2026-02-24 12:00:41 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Minha idade é:"+ 42 +"anos.");
+		System.out.println("o resultado é:" + ( 3+5 <=22));
+	}
+}
